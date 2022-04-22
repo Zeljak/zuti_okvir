c8f990e6c5df1a33c2c38d314af15c6b7001efdd
Zeljka <z.raic@adriamedia.hr>
2022-04-22 17:10:19 +0200

    first commit

---
+++ _javascript/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello Bulma!');
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
